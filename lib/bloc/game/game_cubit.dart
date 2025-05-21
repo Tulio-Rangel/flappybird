822e53b1e6251149083c93a1d24f9341b58ee62b
Tulio-Rangel <tjrn20@hotmail.com>
2025-05-21 14:14:57 -0500

    Actualizar dependencias de Flutter y ajustar configuraciones de compilación para mejorar la compatibilidad y el rendimiento.

--- lib/bloc/game/game_cubit.dart
+++ lib/bloc/game/game_cubit.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'game_state.dart'; // Importa la definición del estado del juego desde 'game_state.dart'.
 
--- lib/widget/game_over_widget.dart
+++ lib/widget/game_over_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flappy_dash/bloc/game/game_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,30 +12,35 @@ class GameOverWidget extends StatelessWidget {
   /// Construye el widget de la pantalla de "Game Over".
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black54, // Fondo semitransparente negro.
-      child: Center(
-        child: Column(
-          mainAxisSize:
-              MainAxisSize.min, // Ajusta el tamaño de la columna al contenido.
-          children: [
-            // Texto principal que indica "GAME OVER!".
-            const Text(
-              'GAME OVER!',
-              style: TextStyle(
-                color: Colors.white, // Texto en color blanco.
-                fontWeight: FontWeight.bold, // Texto en negrita.
-                fontSize: 38, // Tamaño de fuente grande.
+    return BackdropFilter(
+      filter: ImageFilter.blur(
+          sigmaX: 5, sigmaY: 5), // Aplica un desenfoque al fondo.
+      child: Container(
+        color: Colors.black54, // Fondo semitransparente negro.
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize
+                .min, // Ajusta el tamaño de la columna al contenido.
+            children: [
+              // Texto principal que indica "GAME OVER!".
+              const Text(
+                'GAME OVER!',
+                style: TextStyle(
+                  color: Colors.white, // Texto en color blanco.
+                  fontWeight: FontWeight.bold, // Texto en negrita.
+                  fontSize: 38, // Tamaño de fuente grande.
+                ),
               ),
-            ),
-            const SizedBox(height: 18), // Espaciado entre el texto y el botón.
-            // Botón que permite reiniciar el juego.
-            ElevatedButton(
-              onPressed: () => context.read<GameCubit>().restartGame(),
-              // Llama al método `restartGame` del `GameCubit` para reiniciar el juego.
-              child: const Text('PLAY AGAIN!'), // Texto del botón.
-            ),
-          ],
+              const SizedBox(
+                  height: 18), // Espaciado entre el texto y el botón.
+              // Botón que permite reiniciar el juego.
+              ElevatedButton(
+                onPressed: () => context.read<GameCubit>().restartGame(),
+                // Llama al método `restartGame` del `GameCubit` para reiniciar el juego.
+                child: const Text('PLAY AGAIN!'), // Texto del botón.
+              ),
+            ],
+          ),
         ),
       ),
     );
