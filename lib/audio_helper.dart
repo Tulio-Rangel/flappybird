3c490a8c330b93553fdc151e2b7873cfc4371b54
Tulio-Rangel <tjrn20@hotmail.com>
2025-05-22 10:34:17 -0500

    Actualizar manejo de audio y configuración de localizador de servicios

--- lib/audio_helper.dart
+++ lib/audio_helper.dart
@@ -1,37 +1,54 @@
-import 'package:flutter_soloud/flutter_soloud.dart';
+import 'package:flutter_soloud/flutter_soloud.dart'; // Importa la biblioteca flutter_soloud, que proporciona funcionalidades para manejar audio.
 
 class AudioHelper {
-  late SoLoud _soLoud;
-  late AudioSource _backgroundSource;
-  SoundHandle? _playingBackgroundHandle;
+  late SoLoud _soLoud; // Instancia de SoLoud para manejar el motor de audio.
+  late AudioSource
+      _backgroundSource; // Fuente de audio para el sonido de fondo.
+  SoundHandle?
+      _playingBackgroundHandle; // Identificador del sonido de fondo actualmente en reproducción.
 
-  late AudioSource _scoreSource;
+  late AudioSource
+      _scoreSource; // Fuente de audio para el sonido de puntuación.
 
+  // Inicializa el motor de audio y carga los archivos de audio necesarios.
   Future<void> initialize() async {
-    _soLoud = SoLoud.instance;
+    _soLoud = SoLoud.instance; // Obtiene la instancia singleton de SoLoud.
     if (_soLoud.isInitialized) {
-      return;
+      // Verifica si el motor de audio ya está inicializado.
+      return; // Si ya está inicializado, no hace nada.
     }
-    await _soLoud.init();
-    _backgroundSource = await _soLoud.loadAsset('assets/audio/background.mp3');
-    _scoreSource = await _soLoud.loadAsset('assets/audio/score.mp3');
+    await _soLoud.init(); // Inicializa el motor de audio.
+    _backgroundSource = await _soLoud.loadAsset(
+        'assets/audio/background.mp3'); // Carga el archivo de audio de fondo.
+    _scoreSource = await _soLoud.loadAsset(
+        'assets/audio/score.mp3'); // Carga el archivo de audio de puntuación.
   }
 
+  // Reproduce el audio de fondo en bucle.
   void playBackgroundAudio() async {
-    _playingBackgroundHandle = await _soLoud.play(_backgroundSource);
-    _soLoud.setProtectVoice(_playingBackgroundHandle!, true);
+    _playingBackgroundHandle = await _soLoud.play(
+        _backgroundSource); // Reproduce el audio de fondo y guarda el identificador.
+    _soLoud.setProtectVoice(_playingBackgroundHandle!,
+        true); // Protege el audio de fondo para evitar que sea interrumpido.
   }
 
+  // Detiene el audio de fondo con un efecto de desvanecimiento.
   void stopBackgroundAudio() {
     if (_playingBackgroundHandle == null) {
-      return;
+      // Verifica si hay un audio de fondo en reproducción.
+      return; // Si no hay audio en reproducción, no hace nada.
     }
 
     _soLoud.fadeVolume(
-        _playingBackgroundHandle!, 0.0, const Duration(milliseconds: 500));
+        _playingBackgroundHandle!,
+        0.0,
+        const Duration(
+            milliseconds:
+                500)); // Reduce el volumen del audio de fondo a 0 en 500 ms.
   }
 
+  // Reproduce el sonido de puntuación.
   void playScoreCollectSound() async {
-    await _soLoud.play(_scoreSource);
+    await _soLoud.play(_scoreSource); // Reproduce el sonido de puntuación.
   }
 }
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,8 +1,11 @@
-import 'package:flappy_dash/audio_helper.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flappy_dash/audio_helper.dart'; // Importa la clase AudioHelper desde el paquete flappy_dash, que probablemente contiene funcionalidades relacionadas con el manejo de audio.
+import 'package:get_it/get_it.dart'; // Importa la biblioteca GetIt, que es un contenedor de inyección de dependencias para gestionar servicios y objetos compartidos.
 
-final getIt = GetIt.instance;
+final getIt = GetIt
+    .instance; // Crea una instancia singleton de GetIt, que se usará para registrar y acceder a servicios en toda la aplicación.
 
 Future<void> setupServiceLocator() async {
-  getIt.registerLazySingleton<AudioHelper>(() => AudioHelper());
+  // Define una función asíncrona que configura el servicio de localización de dependencias.
+  getIt.registerLazySingleton<AudioHelper>(() =>
+      AudioHelper()); // Registra AudioHelper como un singleton perezoso, lo que significa que se creará solo cuando se necesite por primera vez.
 }
